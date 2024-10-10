0e3fd4c84b8fe4dcd2b6ac005f3587d30f4a6543
MrChukwudi <chukwudidesire01@gmail.com>
2024-10-10 02:07:21 -0400

    First Commit with equilateralTraingle

---
+++ TriangleSolverUnitTest/TriangleTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework.Legacy;
+using NUnit.Framework;
+using TriangleSolver;
+
+namespace TriangleSolverUnitTest
+{
+    [TestFixture] //Annotation to indicate a Unit Test Class: Used to mark any class that will contain Unit tests
+  
+        public class TriangleTests
+        {
+            // 1. Valid equilateral triangle test
+            [TestCase(3, 3, 3, ExpectedResult = "A triangle is formed and it is an EQUILATERAL")]
+            public string AnalyzeTriangle_ValidEquilateralTriangle_Test(int firstSide, int secondSide, int thirdSide)
+            {
+                return Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+            }
+
+        }
+    
+
+}
